7d9a556b9af2b037b3a84e85559a3dc91c07377b
arnav-sys <akhil.aggarwal2016@gmail.com>
2020-11-14 18:47:09 +0530

    first commit

---
+++ build/precache-manifest.5add0d447144fef4cc27fe33af543f78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b947b8ebc229ce9709ccfd58adb596f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13ce9a6741a96b32b9cb",
+    "url": "/static/css/main.9ef35990.chunk.css"
+  },
+  {
+    "revision": "a3538ba22666040ac94c",
+    "url": "/static/js/2.2e5e9b05.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2e5e9b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ce9a6741a96b32b9cb",
+    "url": "/static/js/main.fbeb3376.chunk.js"
+  },
+  {
+    "revision": "6ca49defa095afd3e9c1",
+    "url": "/static/js/runtime-main.f9911b94.js"
+  }
+]);
---
+++ src/spotify.js
@@ -0,0 +1,27 @@
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "de45e40083c44d2181796cf48f0f11ac";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
